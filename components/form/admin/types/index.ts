019cd7f55e1a5c9e0db4aaf609d4e111b8c8eae0
Michael Schramm <michael.schramm@gmail.com>
2022-03-01 01:20:03 +0100

    add admin for location field type

--- components/form/admin/types/index.ts
+++ components/form/admin/types/index.ts
@@ -1,33 +1,22 @@
-import React from 'react'
-import { CheckboxType } from './checkbox.type'
-import { DateType } from './date.type'
-import { DropdownType } from './dropdown.type'
-import { EmailType } from './email.type'
-import { HiddenType } from './hidden.type'
-import { LinkType } from './link.type'
-import { NumberType } from './number.type'
-import { RadioType } from './radio.type'
-import { RatingType } from './rating.type'
-import { SliderType } from './slider.type'
-import { TextType } from './text.type'
-import { TextareaType } from './textarea.type'
+import dynamic from 'next/dynamic'
+import { ComponentType } from 'react'
 import { AdminFieldTypeProps } from './type.props'
-import { YesNoType } from './yes_no.type'
 
 export const adminTypes: {
-  [key: string]: React.FC<AdminFieldTypeProps>
+  [key: string]: ComponentType<AdminFieldTypeProps>
 } = {
-  date: DateType,
-  dropdown: DropdownType,
-  checkbox: CheckboxType,
-  email: EmailType,
-  hidden: HiddenType,
-  link: LinkType,
-  number: NumberType,
-  radio: RadioType,
-  rating: RatingType,
-  slider: SliderType,
-  textarea: TextareaType,
-  textfield: TextType,
-  yes_no: YesNoType,
+  checkbox: dynamic<AdminFieldTypeProps>(() => import('./checkbox.type').then(c => c.CheckboxType )),
+  date: dynamic<AdminFieldTypeProps>(() => import('./date.type').then(c => c.DateType )),
+  dropdown: dynamic<AdminFieldTypeProps>(() => import('./dropdown.type').then(c => c.DropdownType )),
+  email: dynamic<AdminFieldTypeProps>(() => import('./email.type').then(c => c.EmailType )),
+  hidden: dynamic<AdminFieldTypeProps>(() => import('./hidden.type').then(c => c.HiddenType )),
+  link: dynamic<AdminFieldTypeProps>(() => import('./link.type').then(c => c.LinkType )),
+  location: dynamic<AdminFieldTypeProps>(() => import('./location.type').then(c => c.LocationType ), { ssr: false }),
+  number: dynamic<AdminFieldTypeProps>(() => import('./number.type').then(c => c.NumberType )),
+  radio: dynamic<AdminFieldTypeProps>(() => import('./radio.type').then(c => c.RadioType )),
+  rating: dynamic<AdminFieldTypeProps>(() => import('./rating.type').then(c => c.RatingType )),
+  slider: dynamic<AdminFieldTypeProps>(() => import('./slider.type').then(c => c.SliderType )),
+  textarea: dynamic<AdminFieldTypeProps>(() => import('./textarea.type').then(c => c.TextareaType )),
+  textfield: dynamic<AdminFieldTypeProps>(() => import('./text.type').then(c => c.TextType )),
+  yes_no: dynamic<AdminFieldTypeProps>(() => import('./yes_no.type').then(c => c.YesNoType )),
 }
